9fc53ed3c9a45b204144fa4227727b0c38fe6fc4
aaznive <arthur.aznive@gmail.com>
2025-09-24 13:50:14 -0400

    Add Basic Keyboard Controls
    Added basic controls for turning the camera and moving the player works using WASD.

--- scripts/camera_rig.gd
+++ scripts/camera_rig.gd
@@ -1,12 +1,18 @@
 extends SpringArm3D
 
 @onready var camera: Camera3D = $Camera3D
+@export var turn_rate := 180.0
 
 func _ready() -> void:
-
 	spring_length = camera.position.z
 	
 
 #Called every Frame, 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var look_input := Input.get_vector("view_right", "view_left", "view_down", "view_up" )
+	look_input = turn_rate * look_input * delta
+	rotation_degrees.x += look_input.y
+	rotation_degrees.y += look_input.x
+	rotation_degrees.x = clampf(rotation_degrees.x, -70, 50)
+	
+	
